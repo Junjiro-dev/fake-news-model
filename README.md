697840afc5b70cfc9e97b23424354183e1007cd5
Junjiro <jun_j658@hotmail.com>
2020-10-07 10:18:29 -0500

    conflicts solved

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 # fake-news-model
 Deployment of fake news classifier
-=======
-fake_news_classifier
->>>>>>> r0
--- code/processing/data_management.py
+++ code/processing/data_management.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import joblib
+import os
 
 def load_dataset():
-    data = pd.read_csv("fake_news_classifier\code\datasets\cleaned_train_test_dataset.csv", index_col=0)
+    data = pd.read_csv("code/datasets/cleaned_train_test_dataset.csv", index_col=0)
     return data
 
 def save_pipeline(pipeline_to_save):
-    save_path = "fake_news_classifier\code/trained_models/fake_news_model.pkl"
+    save_path = "code/trained_models/fake_news_model.pkl"
     joblib.dump(pipeline_to_save, save_path)
     print("Saved Pipeline")
 
 
 def load_pipeline():
-    save_path = "fake_news_classifier\code/trained_models/fake_news_model.pkl"
+    save_path = "code/trained_models/fake_news_model.pkl"
     trained_model = joblib.load(save_path)
     print("Pipeline loaded")
     return trained_model
