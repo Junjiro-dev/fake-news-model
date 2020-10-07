5e4ade5bb4a58ec3037c034270131050202bf2bf
Junjiro <jun_j658@hotmail.com>
2020-10-07 12:23:12 -0500

    requirements

--- code/req_test.py
+++ code/req_test.py
@@ -1,6 +1,6 @@
 import requests
 
 data = {"title": "Fuentes cercanas al presidente reportan apocalipsis."}
-response = requests.post("{}/".format("http://127.0.0.1:5000"), json =data )
+response = requests.post("{}/".format("https://felix-obando-tesis.uc.r.appspot.com/"), json =data )
 print("Predicción: ")
 print(response.json())
